fa2d9a5d1e96f4016ed07456070a8a3f8893966b
Fish <conner.leuk@gmail.com>
2020-08-16 18:02:50 +0200

    removed nodejs chart and added canvas

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 var moment = require('moment'); // require
 
 var Canvas = require('canvas');
-const ChartjsNode = require('chartjs-node');
+const Chart = require('chart.js');
 
 exports.run = async (client, message, args) =>{
     
@@ -78,8 +78,10 @@ exports.run = async (client, message, args) =>{
     console.log(Object.keys(nice_data))
     console.log(Object.values(nice_data))
 
+    const canvas = createCanvas(600, 400)
+    const ctx = canvas.getContext('2d')
 
-    var chartJsOptions = {
+    new Chart(ctx, {
         type: 'line',
         
         data: {
@@ -128,32 +130,11 @@ exports.run = async (client, message, args) =>{
                 }]
             }
         }
-    }
+    });
 
-    
-    // 600x600 canvas size
-    var chartNode = new ChartjsNode(600, 600);
-    const imagefile = await chartNode.drawChart(chartJsOptions)
-        .then(() => {
-            // chart is created
-        
-            // get image as png buffer
-            return chartNode.getImageBuffer('image/png');
-        })
-        .then(buffer => {
-            Array.isArray(buffer) // => true
-            // as a stream
-            return chartNode.getImageStream('image/png');
-        })
-        .then(streamResult => {
-            // using the length property you can do things like
-            // directly upload the image to s3 by using the
-            // stream and length properties
-            streamResult.stream // => Stream object
-            streamResult.length // => Integer length of stream
-            // write to a file
-            return chartNode.writeImageToFile('image/png', `${__dirname}/../../images/${message.guild.id}.png`);
-        })
+    var buf = canvas.toBuffer();
+    await fs.writeFile(`${__dirname}/../../images/${message.guild.id}.png`, buf);
+   
 
 
 
