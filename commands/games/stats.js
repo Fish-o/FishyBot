8f6206ab1dc76d25ce18510fb771abb57aa6786f
Fish <conner.leuk@gmail.com>
2020-08-18 23:53:59 +0200

    simple fix

--- commands/games/stats.js
+++ commands/games/stats.js
@@ -62,7 +62,7 @@ exports.run = async (client, message, args) => {
     Embed.setAuthor("Powered by IgniteVR Metrics", 'https://ignitevr.gg/wp-content/uploads/2019/09/primary_Optimized.png', `https://ignitevr.gg/stats/${player_name}`);
     Embed.setColor('#0055ff')
     Embed.setTitle(`**${player_name}**'s echo stats`);
-    Embed.setThumbnail(`${__dirname}/../../images/echo_disc.png`)
+    //Embed.setThumbnail(`${__dirname}/../../images/echo_disc.png`)
     Embed.addFields(
 		{ name: 'Level', value: user_stats.level},
 		{ name: 'Games on record', value: user_stats.game_count},
