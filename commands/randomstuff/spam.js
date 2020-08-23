4e5f5e535294c1c820dbebd159999ddb43f7675a
Fish <conner.leuk@gmail.com>
2020-08-23 20:41:29 +0200

    changed spam

--- commands/randomstuff/spam.js
+++ commands/randomstuff/spam.js
@@ -3,7 +3,15 @@ exports.run = (client, message, args) => {
     function myFunc() {
         message.author.send('spam')
     }
+
     var i =0;
+    const m = 50;
+    const s = 50;
+
+    const msges =  m + 2.0 * s * (Math.random() + Math.random() + Math.random() - 1.5);
+    let cosnt =Math.max(5, Math.round(msges))
+
+    message.channel.send(`${rounded} messages comming up!`)
     for(i = 0; i < 60; i++){
         setTimeout(myFunc, 1010*i);
     }
@@ -21,6 +29,6 @@ const path = require("path")
 exports.help = {
     category: __dirname.split(path.sep).pop(),
     name:"spam",
-    description: "Gives the info of the bot",
-    usage: "!info"
+    description: "Sends a random amount of messages to the user who called it in dm's",
+    usage: "!spam"
 };
