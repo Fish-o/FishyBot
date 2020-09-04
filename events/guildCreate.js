e3261cddb9c7187bd153176fa7b7a583c47fd9da
Fish <conner.leuk@gmail.com>
2020-09-04 22:28:36 +0200

    fixed the bot readding a db entry, after rejoining

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,13 +5,10 @@ module.exports = (client, guild) => {
     client.sendinfo('event: guild create')
     console.log(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
 
-    let rawdata = fs.readFileSync(__dirname + '/../jsonFiles/config.json');
-	let config = JSON.parse(rawdata);
-	
 	var guildID = guild.id;
     //var guild = client.guilds.cache.get(guildID);
     
-    const uri = config['dbpath'];
+    const uri = client.config.dbpath;
 
 
     // Get guilds
@@ -25,55 +22,87 @@ module.exports = (client, guild) => {
 			}*/
 
     //old_db_guild = old_db_data.filter(in_guild);
-    guild.members.fetch().then((member_list) => {
-    
-        var guildObject = {
-                id : guild.id,
-                users:{},
-                prefix:"!",
-                settings:{
-                    "dadjokes":false
-                }
-        }
 
-        member_list.forEach(guild_member => {
-            var userObject = {
-            
-                warns:[],
-                data:{
-                    "usernames":{},
-                    "region":null 
-                }
-            
-            }
-            userId = guild_member.id;
-            guildObject.users[userId] = userObject;
-
-        })
-        
-
-
-        // Push new Guild object with users to db
-        const mongoClient = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    var guild_data_promise = new Promise(function(resolve, reject){
+        var mongoClient = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
         mongoClient.connect(err => {
-            if (err) console.log(err);
+            console.log('...conect');
+            if (err) throw err;
             const collection = mongoClient.db("botdb").collection("v2");
-            // perform actions on the collection object
-            collection.insertOne(guildObject, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                mongoClient.close();
-                client.recache()
-            });
+            collection.find({id:guild.id}).toArray(function(err, result) {
+                console.log('...find');
+                if (err) {console.error(err); throw err};
+                
             
+                
+                //let new_results = [];
+                //for(i = 0; i < result.length; i++){
+                //	new_results.push(JSON.stringify({	id: result[i].id,
+                //						guild: result[i].guild}));
+                //}
+                mongoClient.close();
+                console.log('...close');
+                setTimeout(function(){
+                    resolve(result);
+                }, 100);
+                    
+            });
         });
+    });
 
 
-    })
-    
+
+    // Get guilds
+    guild_data_promise.then( async function(value) {
+        value = value[0]
+        if(!value){
 
 
-    
-    
+            guild.members.fetch().then((member_list) => {
+            
+                var guildObject = {
+                        id : guild.id,
+                        users:{},
+                        prefix:"!",
+                        settings:{
+                            "dadjokes":false
+                        }
+                }
 
+                member_list.forEach(guild_member => {
+                    var userObject = {
+                    
+                        warns:[],
+                        data:{
+                            "usernames":{},
+                            "region":null 
+                        }
+                    
+                    }
+                    userId = guild_member.id;
+                    guildObject.users[userId] = userObject;
+
+                })
+                
+
+
+                // Push new Guild object with users to db
+                const mongoClient = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+                mongoClient.connect(err => {
+                    if (err) console.log(err);
+                    const collection = mongoClient.db("botdb").collection("v2");
+                    // perform actions on the collection object
+                    collection.insertOne(guildObject, function(err, res) {
+                        if (err) throw err;
+                        console.log("1 document inserted");
+                        mongoClient.close();
+                        client.recache()
+                    });
+                    
+                });
+
+
+            })
+        }        
+    });
 };
