260c5a355c56dbd74762c16db8675e485e29327f
Fish <conner.leuk@gmail.com>
2020-09-05 17:39:55 +0200

    finished log command

--- commands/admin/logging.js
+++ commands/admin/logging.js
@@ -6,24 +6,50 @@ exports.run = (client, message, args) => {
 
     channel = message.mentions.channels.first();
     if(args[0].toLowerCase() == 'off' || args[0].toLowerCase() == 'stop' || args[0].toLowerCase() == 'close'){
+        guild.fetchWebhooks()
+        .then(webhooks => {
+            const created_webhooks = webhooks.find(webhook => webhook.client.id == client.id)
+            if(created_webhooks){
+                created_webhooks.forEach(webhook => {
+                    webhook.delete()
+                });
+                var query = {id: message.guild.id};
+                const locate_string = "logging"
+                const db_data = undefined;
+                var values = { $set: {[locate_string]:db_data}}
+                client.updatedb(query, value, 'Stopped logging', message.channel)
+            }
+
+        }).catch(console.error);
 
     } else if (channel){
-        
-        channel.createWebhook("FishyBot Logging")
-        .then(webhook => webhook.edit("FishyBot Logging", client.user.avatarURL)
-        .then(wb => {
-            var query = {id: message.guild.id};
-            const locate_string = "logging.webhook"
-            const db_data = {
-                webhook: {
-                    id: wb.id,
-                    token: wb.token
-                }, 
-                channel_id: channel.id
-            }
-            var values = { $set: {[locate_string]:db_data}}
-            client.updatedb(query, value, 'Started logging!', message.channel)
-        }).catch(console.error))
+        guild.fetchWebhooks()
+            .then(webhooks => {
+                const created_webhooks = webhooks.find(webhook => webhook.client.id == client.id)
+                if(created_webhooks){
+                    created_webhooks.forEach(webhook => {
+                        webhook.delete()
+                    });
+                }
+                channel.createWebhook('FishyBot-log', {
+                        avatar: client.user.avatarURL,
+                        reason: 'used to post log events'
+                    })
+                    //.then(webhook => webhook.edit("FishyBot Logging", )
+                    .then(wb => {
+                        var query = {id: message.guild.id};
+                        const locate_string = "logging"
+                        const db_data = {
+                            webhook: {
+                                id: wb.id,
+                                token: wb.token
+                            }, 
+                            channel_id: channel.id
+                        }
+                        var values = { $set: {[locate_string]:db_data}}
+                        client.updatedb(query, value, 'Started logging!', wb)
+                    }).catch(console.error)
+            }).catch(console.error);
     }
 }
 
