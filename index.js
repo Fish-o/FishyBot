17e2d3c5d179e155707342d61a9fdce815bb3e4f
Fish <conner.leuk@gmail.com>
2020-09-05 18:43:48 +0200

    started on actually logging the events

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ client.on('WEBHOOKS_UPDATE', function(client, channel){
     const TEXT = "Webhook updated"
 
     const Discord = require('discord.js');
-    const cache_raw = fs.readFileSync(__dirname + '/../../jsonFiles/exports.conf');
+    const cache_raw = fs.readFileSync(__dirname + '/jsonFiles/cache.js');
     const cache = JSON.parse(cache_raw);
     const cache_guild = cache.data.find(guild => guild.id == channel.guild.id);
 
@@ -235,7 +235,98 @@ client.on('WEBHOOKS_UPDATE', function(client, channel){
 
 }.bind(null, client));
 
+bot.on('guildMemberUpdate', function(guild, oldMember, newMember) {
+    const uri = client.config.dbpath;
+    var mongoClient = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoClient.connect(err => {
+        if (err) throw err;
+        const collection = mongoClient.db("botdb").collection("v2");
+        collection.findOne({id:guild.id}).toArray(function(err, db_guild) {
+            if (err) {console.error(err); throw err};
+            mongoClient.close();
+            if(!db_guild.logging) return;
+            if(!db_guild.logging.webhook.id) return;
+            
+            const log = new Discord.WebhookClient(db_guild.logging.webhook.id, db_guild.logging.webhook.token);
 
+    
+            //var log = guild.channels.find('id', CHANNEL);
+
+            //declare changes
+            var Changes = {
+                unknown: 0,
+                addedRole: 1,
+                removedRole: 2,
+                username: 3,
+                nickname: 4,
+                avatar: 5
+            };
+            var change = Changes.unknown;
+
+            //check if roles were removed
+            var removedRole = '';
+            oldMember.roles.every(function(value) {
+                if(newMember.roles.find('id', value.id) == null) {
+                    change = Changes.removedRole;
+                    removedRole = value.name;
+                }
+            });
+
+            //check if roles were added
+            var addedRole = '';
+            newMember.roles.every(function(value) {
+                if(oldMember.roles.find('id', value.id) == null) {
+                    change = Changes.addedRole;
+                    addedRole = value.name;
+                }
+            });
+
+            //check if username changed
+            if(newMember.user.username != oldMember.user.username)
+                change = Changes.username;
+
+            //check if nickname changed
+            if(newMember.nickname != oldMember.nickname)
+                change = Changes.nickname;
+
+            //check if avatar changed
+            if(newMember.user.avatarURL != oldMember.user.avatarURL)
+                change = Changes.avatar;
+
+            //post in the guild's log channel
+            
+            if (log != null) {
+                switch(change) {
+                    case Changes.unknown:
+                        log.send('**[User Update]** ' + newMember);
+                        break;
+                    case Changes.addedRole:
+                        log.send('**[User Role Added]** ' + newMember + ': ' + addedRole);
+                        break;
+                    case Changes.removedRole:
+                        log.send('**[User Role Removed]** ' + newMember + ': ' + removedRole);
+                        break;
+                    case Changes.username:
+                        log.send('**[User Username Changed]** ' + newMember + ': Username changed from ' +
+                            oldMember.user.username + '#' + oldMember.user.discriminator + ' to ' +
+                            newMember.user.username + '#' + newMember.user.discriminator);
+                        break;
+                    case Changes.nickname:
+                        log.send('**[User Nickname Changed]** ' + newMember + ': ' +
+                            (oldMember.nickname != null ? 'Changed nickname from ' + oldMember.nickname +
+                                + newMember.nickname : 'Set nickname') + ' to ' +
+                            (newMember.nickname != null ? newMember.nickname + '.' : 'original username.'));
+                        break;
+                    case Changes.avatar:
+                        log.send('**[User Avatar Changed]** ' + newMember);
+                        break;
+                }
+            }
+        });
+    });
+    
+
+});
 
 events.misc
 events.server
