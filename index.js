3701af20184638ff6d549e5bab559e64f047926a
Fish <conner.leuk@gmail.com>
2020-09-06 14:33:39 +0200

    fix?

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ client.on('guildMemberUpdate', function(guild, oldMember, newMember) {
         const collection = mongoClient.db("botdb").collection("v2");
         collection.findOne({id:guild.id}, function(err, db_guild) {
             if (err) {console.error(err); throw err};
-            mongoClient.close();
+            
             if(!db_guild.logging) return;
             if(!db_guild.logging.webhook.id) return;
             
@@ -322,6 +322,7 @@ client.on('guildMemberUpdate', function(guild, oldMember, newMember) {
                         break;
                 }
             }
+            mongoClient.close();
         });
     });
     
