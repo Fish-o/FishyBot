0679607a9edf7ee0d6622159a447209f52deeece
Fish <conner.leuk@gmail.com>
2020-09-15 19:15:55 +0200

    added custom commands command

--- commands/admin/custom_commands.js
+++ commands/admin/custom_commands.js
@@ -5,9 +5,6 @@ custom commands
 
 
 input
-	!asdf
-	asdf
-	i like asdf and also fdsa!
 	regex
 
 
@@ -25,8 +22,68 @@ output
 */
 
 
-exports.run = (client, message, args) => {
+exports.run = async function (client, message, args) {
+    `rand: {r10|20} {r10|20} {r10|20} rand done. w8 5 secs {w5} {user} {mention}`;
+
     
+    message.channel.send("What should be the command name? , type `cancel` at any time to stop\nThe command name supports **regex** (dont need the //), to make it activate only at the beginning of a message use `^commandname`");
+    var command_name_raw_msg = await message.channel.awaitMessages(m => m.author.id == message.author.id, {max: 1, time: 120000})
+    
+    let command_name_raw = command_name_raw_msg.first().content;
+
+    if(!command_name_raw_msg.first().mentions.channels.first()){
+        if(command_name_raw.toLowerCase() == 'cancel'){return message.channel.send('Stopped')};
+    }
+
+    let isRegex = true;
+    try {
+        new RegExp(command_name_raw);
+    } catch(e) {
+        isRegex = false;
+    }
+    if(isRegex) {
+        var done = false;
+        var proceed = true;
+        var responses = []
+        while (!done){
+            message.channel.send("What should the response be? , type `cancel` at any time to stop\nExample command response: `Love meter between {user} and {mention}{w1}calculating...{w2}Their love is {r0|100}%`\n{user} - The user who called the command\n{mention} - the user who was mentioned in the message\n{w3} - waights for 3 seconds before sending the rest of the message\n{r0|100} - returns a random number between 0 and 100");
+            var command_response_msg = await message.channel.awaitMessages(m => m.author.id == message.author.id, {max: 1, time: 120000})
+            
+            let command_response = command_response_msg.first().content;
+
+            if(!command_response_msg.first().mentions.channels.first()){
+                if(command_response.toLowerCase() == 'cancel'){proceed = false; return message.channel.send('Stopped')};
+            }
+            
+            responses.push(command_response)
+            
+
+            var addnothermsglooop = true;
+
+            while(addnothermsglooop == true){
+                message.channel.send("Do you want to add another respons? ( _yes_ or _no_ )");
+                var another_msg_raw = await message.channel.awaitMessages(m => m.author.id == message.author.id, {max: 1, time: 120000})
+                let another_msg = another_msg_raw.first().content;
+                if(!another_msg_raw.first().mentions.channels.first()){
+                    if(another_msg.toLowerCase() == 'cancel'){proceed = false;addnothermsglooop = false;done = false; return message.channel.send('Stopped')};
+                }else if(another_msg.toLowerCase() == 'yes'){
+                    addnothermsglooop = false;
+                }else if(another_msg.toLowerCase() == 'no'){
+                    done = true;
+                    addnothermsglooop = false;
+                }
+            }
+        }
+        if(proceed){
+
+            const locate = "custom_commands."+command_name_raw;
+            const value = {$set: {[locate]:responses}};
+            client.updatedb({id:message.guild.id}, value, 'Added custom command!', message.channel)
+
+        }
+    }else return message.channel.send('That command name is invalid!');
+    
+
 }
 
 exports.conf = {
@@ -59,7 +116,8 @@ Make new and delete commands
 !custom delete (command name/index)
 
 Add and remove responses to a command
-!custom add (command name/index) (resoponse index)
+!custom set (command name/index) data
+~~!custom add (command name/index) (response index)~~
 !custom remove (command name/index) (response index)
 
 
