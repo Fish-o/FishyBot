c2cbb915d78e29b90a01e093efbefa5aa427b8d0
Fish <conner.leuk@gmail.com>
2020-09-26 15:40:39 +0200

    canvacord then!?

--- commands/levels/showrank.js
+++ commands/levels/showrank.js
@@ -1,98 +1,59 @@
-const Discord = require('discord.js');
-var Canvas = require('canvas');
+const Canvacord = require("canvacord");
+const { MessageAttachment } = require("discord.js");
 
-async function GetAvatar(client, message, ctx) {
-    //I HATE this part. I think here at the Yukiko Dev Team Incorporation ltd llc Group we all hate this part.
-    // If Anyone know how to use canvas and this part better than us, please feel free to PR :) 
-    // (or if you are a dev of node-canvas please contact me on Twitter/Github @Asthriona )
-
-    //Avatar function
-    //get user in the database.
-    client.dbgetuser(client, message.guild.id, message.author.id).then(users=>{
-        //XP bar calculus. (Help)
-        let nxtlvl = 300 * Math.pow(2, users.level);
-        var n = ((users.xp - nxtlvl) / nxtlvl) * -100;
-        var member = client.getMember(message);
-        var arc = (100-n)/100*Math.PI;
-        //Image arc draw (Cry in js)
-        ctx.strokeStyle = member.displayHexColor;
-        ctx.beginPath();
-        ctx.lineWidth = 15;
-        ctx.arc(125, 140, 102, Math.PI*1.5,arc);
-        ctx.stroke();
-    })
-    //lvlbar background
-            ctx.strokeStyle = "rgba(0, 0, 0, 0.8)";
-            ctx.beginPath();
-            ctx.lineWidth = 20;
-            ctx.arc(125, 140, 102, 0,Math.PI * 2);
-            ctx.stroke();
-    //Print the profile picture.
-    var avatar = await Canvas.loadImage(message.author.displayAvatarURL);
-    ctx.beginPath();
-    ctx.arc(125, 140, 100, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.clip();
-    ctx.drawImage(avatar, 25, 40, 200, 200);
+function match(msg, i) {
+    if (!msg) return undefined;
+    if (!i) return undefined;
+    let user = i.members.cache.find(
+        m =>
+            m.user.username.toLowerCase().startsWith(msg) ||
+            m.user.username.toLowerCase() === msg ||
+            m.user.username.toLowerCase().includes(msg) ||
+            m.displayName.toLowerCase().startsWith(msg) ||
+            m.displayName.toLowerCase() === msg ||
+            m.displayName.toLowerCase().includes(msg)
+    );
+    if (!user) return undefined;
+    return user.user;
 }
+
+
+
+
+
+
 
 exports.run = async (client, message, args) => {
     message.channel.startTyping();
-    var member = client.getMember(message, args.join(" "));
-
-    var canvas = Canvas.createCanvas(934, 282);
-    var ctx = canvas.getContext('2d');
-
-
-    let users = await client.dbgetuser(client, message.guild.id, member.id)
+    let user =
+        message.mentions.users.first() ||
+        client.users.cache.get(args[0]) ||
+        match(args.join(" ").toLowerCase(), message.guild) ||
+        message.author;
+    let canvas = new Canvacord();
     
-    //ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    //Draw rectangle
-    ctx.beginPath();
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
-    ctx.fillRect(260, 80, 650, 160);
-    ctx.closePath();
-    ctx.stroke();
-    //Get Username
-    //ctx.font = '60px sans-serif';
-    ctx.fillStyle = '#fff';
-    ctx.fillText(member.displayName, 280, 136);
-    //Show XP and levels
-    let nxtlvl = 300 * Math.pow(2, users.level);
-    //ctx.font = '40px sans-serif';
-    ctx.fillStyle = '#fff';
-    ctx.fillText("You are level " + users.level + " - " + users.xp + " XP", 280, 180);
-    //xp left
-    var xpLeft = nxtlvl - users.xp;
-    //ctx.font = '50px sans-serif';
-    ctx.fillStyle = '#fff';
-    ctx.fillText(`next level in ${xpLeft} XP`, 280, 225);
-    //Get avatar
-    await GetAvatar(client, message, ctx);
-    //Put all the things together and send it in a nice package.
-    var lvlimg = new Discord.Attachment(canvas.toBuffer(), 'rank-cards.png');
-    message.channel.send('asdfasdf', lvlimg)
+    const db_user = await client.dbgetuser(client, message.guild.id, user.id);
+    let level = db_user.rank.level || 0;
+    level = level.toString();
 
-    /*const constant = 0.5; 
+    let exp = db_user.rank.exp || 0;
+    let neededXP = Math.floor(Math.pow(level / 0.1, 2));
 
-    let level = Math.floor(constant * Math.sqrt(user.xp))
+    let rank = 2
+    rank = rank.toString();
 
-    let xp_for_curlvl = Math.floor(Math.pow(level * constant,2));
-    let xp_for_nextlvl = Math.floor(Math.pow(level+1 * constant,2));
-    let diff = xp_for_nextlvl-xp_for_curlvl;
+    let img = await canvas.rank({
+        username: user.username,
+        discrim: user.discriminator,
+        currentXP: exp.toString(),
+        neededXP: neededXP.toString(),
+        rank,
+        level,
+        avatarURL: user.displayAvatarURL({ format: "png" }),
+        background: "https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?ixlib=rb-1.2.1&w=1000&q=80"
+      });
+      return message.channel.send(new MessageAttachment(img, "rank.png"));
 
-    const embed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(`${message.author.name}'s stats`)
-    .addField('Level', level)
-    .addField('Xp', user.xp)
-    .addField('Xp for this lvl', xp_for_curlvl)
-    .addField('Xp for next', xp_for_nextlvl)
-    .addField('Xp till nex', diff)
-    .setTimestamp();
-
-
-    message.channel.send(embed);*/
     message.channel.stopTyping();
     
     
