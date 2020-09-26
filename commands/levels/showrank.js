269485f58361af1fcbd07e4d6c927937e4590f64
Fish <conner.leuk@gmail.com>
2020-09-26 13:46:27 +0200

    send embed

--- commands/levels/showrank.js
+++ commands/levels/showrank.js
@@ -1,9 +1,29 @@
+const Discord = require('discord.js');
 exports.run = (client, message, args) => {
     message.channel.startTyping();
     client.dbgetuser(client, message.guild.id, message.author.id).then(user=>{
 
 
-        message.channel.send(user.xp)
+        const constant = 0.5; 
+
+        let level = Math.floor(constant * Math.sqrt(user.xp))
+
+        let xp_for_curlvl = Math.floor(Math.pow(level / constant,2));
+        let xp_for_nextlvl = Math.floor(Math.pow(level+1 / constant,2));
+        let diff = xp_for_nextlvl-xp_for_curlvl;
+
+        const exampleEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`${message.author.name}'s stats`)
+        .addField('Level', level)
+        .addField('Xp', user.xp)
+        .addField('Xp for this lvl', xp_for_curlvl)
+        .addField('Xp for next', xp_for_nextlvl)
+        .addField('Xp till nex', diff)
+        .setTimestamp();
+
+
+        message.channel.send(embed);
         message.channel.stopTyping();
     })
     
