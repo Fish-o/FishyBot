ddec0cbd50ee41b8b21f1a60f804aacc4bc2918b
Fish <64684124+Fish-o@users.noreply.github.com>
2020-10-16 23:51:02 +0200

    Update levelspam.js

--- commands/levels/levelspam.js
+++ commands/levels/levelspam.js
@@ -1,5 +1,5 @@
 const  Guild = require('../database/schemas/Guild');
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
     if(!message.mentions.channels.first())
         return message.channel.send('You did not specify a valid channel');
 
