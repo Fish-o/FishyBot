f0dce90e84498f0060f622fe4c7e4ab613d44b93
Fish <conner.leuk@gmail.com>
2020-10-17 17:08:58 +0200

    fix for it not being disabled

--- events/level.js
+++ events/level.js
@@ -22,43 +22,44 @@ exports.event = async (client, message) =>{
             const member = message.member;
             const guild = message.guild;
             const dbGuild = await Guild.findOne({id:guild.id})
-
-            if(dbGuild.settings.levels == false){
-                return
-            }
-            if(!(member.id in dbGuild.levels.members)){
-                return await Guild.update({id:guild.id}, {['levels.members.'+member.id]: {level:1, exp:1}})
-            }
-
-
-            const dbUserLevelData = dbGuild.levels.members[member.id];
-            let level = dbUserLevelData.level;
-            let exp = dbUserLevelData.exp;
-
-            let neededXP = Math.floor(Math.pow(level / 0.5, 2));
-
-            if(exp >= neededXP){
-
-                level = level+1
-                exp = 1
-                await Guild.update({id:guild.id}, {['levels.members.'+member.id]: {level, exp}})
-
-                let lvlUpMsg = dbGuild.levels.lvlUpMsg || `Congratulations {member}, you leveled up to level {level}`;
-                lvlUpMsg = lvlUpMsg.replace(/\{member\}/g, `<@${member.id}>`)
-                lvlUpMsg = lvlUpMsg.replace(/\{level\}/g, level)
-
-                if(dbGuild.levels.channel){
-                    if(guild.channels.cache.has(dbGuild.levels.channel)){
-                        guild.channels.cache.get(dbGuild.levels.channel).send(lvlUpMsg);
-                    } else{
-                        message.channel.send(`<@${member.id}> has leveld up to level ${level}, but i couldnt find the channel where this message was supposed to be sent to. Please contact an admin of your server. `)
-                    }
-                    
-                } else{
-                    message.channel.send(lvlUpMsg)
+            console.log(dbGuild.settings.get('levels'))
+            if(dbGuild.settings.get('levels') == false){
+                return;
+            } else {
+                if(!(member.id in dbGuild.levels.members)){
+                    return await Guild.update({id:guild.id}, {['levels.members.'+member.id]: {level:1, exp:1}})
                 }
-            } else{
-                await Guild.update({id:guild.id}, {$inc: {['levels.members.'+member.id+'.exp']: 1}})
+
+
+                const dbUserLevelData = dbGuild.levels.members[member.id];
+                let level = dbUserLevelData.level;
+                let exp = dbUserLevelData.exp;
+
+                let neededXP = Math.floor(Math.pow(level / 0.5, 2));
+
+                if(exp >= neededXP){
+
+                    level = level+1
+                    exp = 1
+                    await Guild.update({id:guild.id}, {['levels.members.'+member.id]: {level, exp}})
+
+                    let lvlUpMsg = dbGuild.levels.lvlUpMsg || `Congratulations {member}, you leveled up to level {level}`;
+                    lvlUpMsg = lvlUpMsg.replace(/\{member\}/g, `<@${member.id}>`)
+                    lvlUpMsg = lvlUpMsg.replace(/\{level\}/g, level)
+
+                    if(dbGuild.levels.channel){
+                        if(guild.channels.cache.has(dbGuild.levels.channel)){
+                            guild.channels.cache.get(dbGuild.levels.channel).send(lvlUpMsg);
+                        } else{
+                            message.channel.send(`<@${member.id}> has leveld up to level ${level}, but i couldnt find the channel where this message was supposed to be sent to. Please contact an admin of your server. `)
+                        }
+                        
+                    } else{
+                        message.channel.send(lvlUpMsg)
+                    }
+                } else{
+                    await Guild.update({id:guild.id}, {$inc: {['levels.members.'+member.id+'.exp']: 1}})
+                }
             }
         }
     }
